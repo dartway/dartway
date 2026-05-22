1a92c8c47dd3b5df9df4090ce0a257803b088895
Evgenii Novikov <disregard.the.rest@gmail.com>
2026-05-22 05:01:20 +0300

    repo reorder

--- templates/dartway_project_template/project_name_flutter/android/app/src/main/kotlin/com/example/project_name_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.project_name_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- templates/dartway_project_template/project_name_server/lib/src/dartway/dartway_session_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:project_name_server/src/generated/protocol.dart';
-import 'package:serverpod/serverpod.dart';
-
-import 'dartway_core.dart';
-
-extension DwSessionExtension on Session {
-  Future<UserProfile?> get currentUserProfile => dw.currentUserProfile(this);
-}
